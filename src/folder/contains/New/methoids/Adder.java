afa8f74e2e5e2324e3179b3494cf92abc6e6f33f
Kivitoe <jwkirk825@gmail.com>
2016-02-12 10:29:54 -0600

    Create Adder.java

---
+++ src/folder/contains/New/methoids/Adder.java
@@ -0,0 +1,11 @@
+package folder.contains.New.methoids;
+
+/* This part was discarded. This may be used in a later version. */
+
+public class Adder {
+	
+	public static void setInts(int int1, int int2, int int3){
+		int3 = int1 + int2;
+	}
+
+}
